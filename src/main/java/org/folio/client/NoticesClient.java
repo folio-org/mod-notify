97dec767061960824c6f5214c1c50bce5efd0b11
Mukhiddin Yusupov <mukhiddin_yusupov@epam.com>
2026-02-03 13:56:38 +0500

    MODNOTIFY-148: upgrade module to vert.x 5.0

--- src/main/java/org/folio/client/NoticesClient.java
+++ src/main/java/org/folio/client/NoticesClient.java
@@ -20,10 +20,7 @@ import org.folio.rest.jaxrs.model.TemplateProcessingRequest;
 import org.folio.rest.jaxrs.model.TemplateProcessingResult;
 
 import io.vertx.core.Future;
-import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.ext.web.client.HttpResponse;
 
 public class NoticesClient extends OkapiClient {
   private static final Logger log = LogManager.getLogger(NoticesClient.class);
@@ -34,13 +31,10 @@ public class NoticesClient extends OkapiClient {
 
   public Future<EventEntity> getEventConfig(String name) {
     log.debug("getEventConfig:: parameters name: {}", name);
-    Promise<HttpResponse<Buffer>> promise = Promise.promise();
-    getAbs("/eventConfig")
+    return getAbs("/eventConfig")
       .addQueryParam("query", "name==" + name)
       .putHeader(ACCEPT, APPLICATION_JSON)
-      .send(promise);
-
-    return promise.future()
+      .send()
       .map(responseMapper(EventEntityCollection.class))
       .map(collection -> collection.getEventEntity()
         .stream()
@@ -51,13 +45,10 @@ public class NoticesClient extends OkapiClient {
 
   public Future<TemplateProcessingResult> postTemplateRequest(TemplateProcessingRequest request) {
     log.debug("postTemplateRequest:: parameters request: {}", () -> asJson(request));
-    Promise<HttpResponse<Buffer>> promise = Promise.promise();
-    postAbs("/template-request")
+    return postAbs("/template-request")
       .putHeader(ACCEPT, APPLICATION_JSON)
       .putHeader(CONTENT_TYPE, APPLICATION_JSON)
-      .sendJson(request, promise);
-
-    return promise.future()
+      .sendJson(request)
       .onSuccess(r -> log.info("postTemplateRequest:: Posted Template Request Successfully"))
       .map(responseMapper(TemplateProcessingResult.class))
       .onSuccess(r -> log.info("postTemplateRequest:: Posted Template Request Successfully"));
@@ -65,12 +56,9 @@ public class NoticesClient extends OkapiClient {
 
   public Future<Void> postMessageDelivery(NotifySendRequest request) {
     log.debug("postMessageDelivery:: parameters request: {}", () -> asJson(request));
-    Promise<HttpResponse<Buffer>> promise = Promise.promise();
-    postAbs("/message-delivery")
+    return postAbs("/message-delivery")
       .putHeader(ACCEPT, TEXT_PLAIN)
-      .sendJson(request, promise);
-
-    return promise.future()
+      .sendJson(request)
       .onSuccess(r -> log.info("postMessageDelivery:: result: {}", () -> bodyAsString(r)))
       .map(responseMapper(Void.class));
   }
